5b90bb87462ff014dd25bf5e1dee3f30db9c3f0b
minghu6 <a19678zy@163.com>
2017-12-07 22:53:10 +0800

    basic implement
    (cherry picked from commit ace27ec)

--- union_find.py
+++ union_find.py
@@ -2,57 +2,141 @@
 """
 
 """
-from collections import OrderedDict
+from copy import copy
 
 
-class UnionFind:
+def _isiterable(obj, but_str_bytes=True):
+    """
+    :param obj:
+    :param but_str_bytes: most of time, we don't need str and bytes
+    :return:
+    """
+    from collections import Iterable
+    if but_str_bytes and isinstance(obj, (str, bytes, bytearray)):
+        return False
+    else:
+        return isinstance(obj, Iterable)
 
-    def __init__(self, data=None, parent=None, sub=OrderedDict()):
+
+class UnionFind(object):
+    """
+    This design don't consider the balance of union find tree (because it's difficult to maintain the rank),
+    may be need to be improved in the future!
+    """
+
+    def __init__(self, data=None, parent=None, subs=list()):
         self._data = data
         self._parent = parent
-        self._sub = sub
-
-        pass
+        self._subs = subs
 
     def __getstate__(self):
-        if len(self) == 0:
-            # The state can't be an empty list.
-            # We need to return a true value, or else __setstate__ won't be run.
-            #
-            # This could have been done more gracefully by always putting the state
-            # in a tuple, but this way is backwards- and forwards- compatible with
-            # previous versions of OrderedSet.
-            return (None,)
-        else:
-            return list(self)
+        return self._data, self._parent, self._subs
 
     def __setstate__(self, state):
-        if state == (None,):
-            self.__init__([])
+        self.__init__(*state)
+
+    def __eq__(self, other):
+        if self is other:
+            return True
+
+        if other is None:
+            return False
+
+        # We can ensure that the max rank of union-find <= 2
+        if self._parent is None and other.parent is None:
+            if self._data != other.data:
+                return False
+            if len(self._subs) != len(other.subs):
+                return False
+            for self_subs, other_subs in zip(self._subs, other.subs):
+                if self_subs.data != other_subs.data:
+                    return False
+            return True
+        elif self._parent is not None and other.parent is not None:
+            return self._parent == other.parent
         else:
-            self.__init__(state)
-
-    def __add__(self, other):
-        pass
-
-    def __iadd__(self, other):
-        pass
+            return False
 
     def __str__(self):
-        pass
+        return ("parent: {0}\n"
+                "data:   {1}\n"
+                "subs:    {2}\n".format(self._parent, self._data, self._subs))
 
-    def _find(self):
-        pass
-
-    def same(self):
-        pass
-
-    def unite(self):
-        pass
+    def get_data(self):
+        return copy(self._data)
     
-    def create_sub_union_find(self):
-        pass
-        
+    def set_data(self, data):
+        self._data = data
+    
+    data = property(get_data, set_data)
 
-def union_find(*args, **kwargs):
-    return UnionFind(*args, **kwargs)
+    def get_parent(self):
+        return self._parent
+
+    def set_parent(self, parent):
+        """the unique parent entry to ensure that no-indirect-parent"""
+        self._parent = parent
+
+        if self._parent is not None:
+            self._compress_path()
+
+    def _compress_path(self):
+        for item in self._subs:
+            item.parent = self._parent
+        self._subs = list()
+
+    parent = property(get_parent, set_parent)
+
+    def get_subs(self):
+        return self._subs
+
+    subs = property(get_subs)
+
+    def same(self, other_union_finds):
+        if not _isiterable(other_union_finds):
+            other_union_finds = [other_union_finds]
+
+        for each_other_union_find in other_union_finds:
+            if each_other_union_find is None:
+                return False
+
+            if self._parent is None:
+                if self == each_other_union_find.parent:
+                    return True
+                else:
+                    return False
+            else:
+                return self._parent.same(each_other_union_find)
+
+    def unite(self, other_union_finds):
+        if not _isiterable(other_union_finds):
+            other_union_finds = [other_union_finds]
+
+        for each_other_union_find in other_union_finds:
+            self._unite(each_other_union_find)
+    
+    def _unite(self, other_union_find):
+        if self.same(other_union_find):
+            return
+
+        other_union_find.parent = self
+        if self.parent is None:
+            self._subs.append(other_union_find)
+    
+    def create_sub_union_find(self, data=None):
+        subs = UnionFind(data)
+        if self._parent is None:
+            subs.parent = self
+        else:
+            subs.parent = self._parent
+
+        if self.parent is None:
+            self._subs.append(subs)
+
+        return subs
+
+    def copy(self):
+        """shallow copy"""
+        
+        return UnionFind(self.data, self.parent, self.subs)
+
--- union_find_test.py
+++ union_find_test.py
@@ -2,20 +2,71 @@
 """
 
 """
-from nose.tools import eq_, raises, assert_raises
-from union_find import UnionFind
+from nose.tools import eq_, assert_true, assert_false
+from union_find import UnionFind, _isiterable
 import pickle
 
 
 def test_pickle():
-    pass
-    # set1 = OrderedSet('abracadabra')
-    # roundtrip = pickle.loads(pickle.dumps(set1))
-    # assert roundtrip == set1
+    union_find1 = UnionFind('root')
+    eq_(pickle.loads(pickle.dumps(union_find1)), union_find1)
+
+    union_find1.create_sub_union_find('sub1')
+    union_find1.create_sub_union_find('sub2')
+    union_find1.parent = UnionFind('root2')
+    eq_(pickle.loads(pickle.dumps(union_find1)), union_find1)
 
 
 def test_empty_pickle():
-    pass
-    # empty_oset = UnionFind()
-    # empty_roundtrip = pickle.loads(pickle.dumps(empty_oset))
-    # assert empty_roundtrip == empty_oset
+    union_find1 = UnionFind()
+    roundtrip = pickle.loads(pickle.dumps(union_find1))
+    eq_(roundtrip, union_find1)
+
+
+def test_same():
+    union_find1 = UnionFind('root', subs=[])
+
+    sub1 = union_find1.create_sub_union_find('sub1')
+    union_find1.parent = UnionFind('root2', subs=[])
+    sub2 = union_find1.create_sub_union_find('sub2')
+
+    assert_true(sub1.same(sub2))
+    assert_true(sub2.same(sub1))
+    assert_true(sub1.same([sub1, sub2]))
+
+    union_find2 = UnionFind('root2', subs=[])
+    assert_false(union_find1.same(union_find2))
+    union_find2.data = 'root'
+    assert_false(union_find1.same(union_find2))
+
+
+def test_union():
+    root1 = UnionFind('root')
+    sub1 = root1.create_sub_union_find('sub1')
+
+    roo2 = UnionFind('root2')
+    sub2 = roo2.create_sub_union_find('sub2')
+
+    sub2.unite(sub1)
+
+    assert_true(sub1.same([sub1, sub2, root1, roo2]))
+
+
+def test_copy():
+    root1 = UnionFind('root')
+    root1.create_sub_union_find('sub1')
+
+    root1_copy = root1.copy()
+    eq_(root1, root1_copy)
+    assert_true(root1 is not root1_copy)
+
+
+def test___str__():
+    root1 = UnionFind('root')
+    root1.create_sub_union_find('sub1')
+
+    isinstance(root1.__str__(), str)
+
+
+def test__isiterable():
+    assert_false(_isiterable('aaa'))
